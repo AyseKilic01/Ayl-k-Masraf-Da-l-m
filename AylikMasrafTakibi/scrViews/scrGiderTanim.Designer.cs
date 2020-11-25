10e24e991f19a0c871614412272c845767954736
Ayse <ayse.klc.95@gmail.com>
2020-11-25 22:29:42 +0300

    ll

--- AylikMasrafTakibi/scrViews/scrGiderTanim.Designer.cs
+++ AylikMasrafTakibi/scrViews/scrGiderTanim.Designer.cs
@@ -40,9 +40,10 @@
             this.colpasif = new DevExpress.XtraGrid.Columns.GridColumn();
             this.parGiderTableAdapter1 = new AylikMasrafTakibi.afbDataSet1TableAdapters.parGiderTableAdapter();
             this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
-            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
-            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnKapat = new DevExpress.XtraEditors.SimpleButton();
+            this.btnKaydKapat = new DevExpress.XtraEditors.SimpleButton();
+            this.btnKaydet = new DevExpress.XtraEditors.SimpleButton();
+            this.colGiderTip = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dsGider)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
@@ -83,10 +84,12 @@
             this.colcode,
             this.colexplanation,
             this.colgidertipi,
+            this.colGiderTip,
             this.colvadetarih,
             this.colpasif});
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Top;
             this.gridView1.OptionsView.ShowGroupPanel = false;
             // 
             // colid
@@ -115,11 +118,9 @@
             // 
             // colgidertipi
             // 
-            this.colgidertipi.Caption = "Gider Tipi";
+            this.colgidertipi.Caption = "colgidertip";
             this.colgidertipi.FieldName = "gidertipi";
             this.colgidertipi.Name = "colgidertipi";
-            this.colgidertipi.Visible = true;
-            this.colgidertipi.VisibleIndex = 2;
             // 
             // colvadetarih
             // 
@@ -143,38 +144,48 @@
             // 
             // groupControl1
             // 
-            this.groupControl1.Controls.Add(this.simpleButton3);
-            this.groupControl1.Controls.Add(this.simpleButton2);
-            this.groupControl1.Controls.Add(this.simpleButton1);
+            this.groupControl1.Controls.Add(this.btnKapat);
+            this.groupControl1.Controls.Add(this.btnKaydKapat);
+            this.groupControl1.Controls.Add(this.btnKaydet);
             this.groupControl1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupControl1.Location = new System.Drawing.Point(0, 0);
             this.groupControl1.Name = "groupControl1";
             this.groupControl1.Size = new System.Drawing.Size(597, 60);
             this.groupControl1.TabIndex = 3;
             // 
-            // simpleButton3
+            // btnKapat
             // 
-            this.simpleButton3.Location = new System.Drawing.Point(223, 24);
-            this.simpleButton3.Name = "simpleButton3";
-            this.simpleButton3.Size = new System.Drawing.Size(75, 23);
-            this.simpleButton3.TabIndex = 2;
-            this.simpleButton3.Text = "Kapat";
+            this.btnKapat.Location = new System.Drawing.Point(223, 24);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(75, 23);
+            this.btnKapat.TabIndex = 2;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
             // 
-            // simpleButton2
+            // btnKaydKapat
             // 
-            this.simpleButton2.Location = new System.Drawing.Point(112, 24);
-            this.simpleButton2.Name = "simpleButton2";
-            this.simpleButton2.Size = new System.Drawing.Size(105, 23);
-            this.simpleButton2.TabIndex = 1;
-            this.simpleButton2.Text = "Kaydet && Kapat";
+            this.btnKaydKapat.Location = new System.Drawing.Point(112, 24);
+            this.btnKaydKapat.Name = "btnKaydKapat";
+            this.btnKaydKapat.Size = new System.Drawing.Size(105, 23);
+            this.btnKaydKapat.TabIndex = 1;
+            this.btnKaydKapat.Text = "Kaydet && Kapat";
+            this.btnKaydKapat.Click += new System.EventHandler(this.btnKaydKapat_Click);
             // 
-            // simpleButton1
+            // btnKaydet
             // 
-            this.simpleButton1.Location = new System.Drawing.Point(31, 24);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
-            this.simpleButton1.TabIndex = 0;
-            this.simpleButton1.Text = "Kaydet";
+            this.btnKaydet.Location = new System.Drawing.Point(31, 24);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 0;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // colGiderTip
+            // 
+            this.colGiderTip.Caption = "Gider Tipi";
+            this.colGiderTip.Name = "colGiderTip";
+            this.colGiderTip.Visible = true;
+            this.colGiderTip.VisibleIndex = 2;
             // 
             // scrGiderTanim
             // 
@@ -210,8 +221,9 @@
         private DevExpress.XtraGrid.Columns.GridColumn colpasif;
         private afbDataSet1TableAdapters.parGiderTableAdapter parGiderTableAdapter1;
         private DevExpress.XtraEditors.GroupControl groupControl1;
-        private DevExpress.XtraEditors.SimpleButton simpleButton3;
-        private DevExpress.XtraEditors.SimpleButton simpleButton2;
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton btnKapat;
+        private DevExpress.XtraEditors.SimpleButton btnKaydKapat;
+        private DevExpress.XtraEditors.SimpleButton btnKaydet;
+        private DevExpress.XtraGrid.Columns.GridColumn colGiderTip;
     }
 }
